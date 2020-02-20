8103fdb28fc768ab8d3003917d4db5e7c93bc534
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-02-20 15:10:32 +0100

    XSS attempt using xlink:href attribute

--- force-app/main/default/lwc/hello/hello.js
+++ force-app/main/default/lwc/hello/hello.js
@@ -1,5 +1,8 @@
 import { LightningElement } from 'lwc';
+import SVG_URL from '@salesforce/resourceUrl/test';
 
 export default class Hello extends LightningElement {
-    greeting = 'World';
+    get svgURL() {
+        return `${SVG_URL}#xss`;
+    }
 }
